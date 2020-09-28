0ea0d1d592e105cb2b3dd3a39021c7bd09faae20
volfan6415 <volfan6415@gmail.com>
2020-09-28 13:05:57 -0500

    Update evmqtt.py
    the on_connect method needs to have the additional variables added to properly update with the newer versions of the paho client.  Once these variables are added the scripts works as intended

--- evmqtt.py
+++ evmqtt.py
@@ -49,7 +49,7 @@ class Watcher:
 # The callback for when the client receives a CONNACK response from the server.
 
 
-def on_connect(client, rc):
+def on_connect(client, userdata, flags, rc):
     log("Connected with result code " + str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
